be86178bf3b766a1cd0186e3c9213be95cc9e668
ghifari <ghifari@fanintek.com>
2024-07-08 15:52:08 +0700

    font size navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = (props: Props) => {
   return (
     <div className='h-10 sticky bg-black content-center'>
       <div className='w-full h-5 flex items-center justify-around px-5'>
-        <div className='flex items-center gap-10 text-slate-400'>
+        <div className='flex text-sm md:text-base items-center gap-10 text-slate-400'>
           <div className='cursor-pointer underline hover:underline-offset-4'>
             About Us
           </div>
@@ -22,7 +22,7 @@ const Navbar = (props: Props) => {
         <div className='hidden md:flex items-center gap-2 text-slate-400 font-mono'>
           Amin Arowana Fish
         </div>
-        <div className='flex items-center gap-2'>
+        <div className='flex text-sm md:text-base items-center gap-2'>
           <div className=' text-slate-100 font-mono bg-gradient-to-r from-red-700 via-pink-700 to-purple-700 p-1 rounded-md text-sm cursor-pointer'>
             Hubungi Saya
           </div>
